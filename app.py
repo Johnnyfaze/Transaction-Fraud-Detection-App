54fa741e6e8ce386b90f62ed94a7ddffc1d16a4d
Johnny <johnnyfaze05@gmail.com>
2025-10-30 12:14:23 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+# libraies importation
+import gradio as gr
+import joblib
+import pandas as pd
+
+# Load the trained pipeline
+pipeline = joblib.load("fraud_detection_model.pkl")
+
+def predict_fraud(type, amount, oldbalanceOrg, newbalanceOrig, oldbalanceDest, newbalanceDest):
+    # Prepare input as a DataFrame
+    input_df = pd.DataFrame([{
+        "type": type,
+        "amount": amount,
+        "oldbalanceOrg": oldbalanceOrg,
+        "newbalanceOrig": newbalanceOrig,
+        "oldbalanceDest": oldbalanceDest,
+        "newbalanceDest": newbalanceDest
+    }])
+    # Predict
+    pred = pipeline.predict(input_df)[0]
+    return "Fraudulent" if pred == 1 else "Legitimate"
+
+types = ["CASH_IN", "CASH_OUT", "DEBIT", "PAYMENT", "TRANSFER"]
+
+demo = gr.Interface(
+    fn=predict_fraud,
+    inputs=[
+        gr.Dropdown(types, label="Transaction Type"),
+        gr.Number(label="Amount ($)"),
+        gr.Number(label="Old Balance Origin Bank"),
+        gr.Number(label="New Balance Origin Bank"),
+        gr.Number(label="Old Balance Destination Bank"),
+        gr.Number(label="New Balance Destination Bank"),
+    ],
+    outputs=gr.Textbox(label="Prediction"),
+    title="Online Fraud Transaction Detection",
+    description="Enter transaction details to predict if it's fraudulent or legitimate."
+)
+# run the app
+if __name__ == "__main__":
+    demo.launch()
